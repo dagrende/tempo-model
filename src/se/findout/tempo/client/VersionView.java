fffee3d2b607ada79a3a4ee297cac3773567c71a
Dag Rende <dag.rende@find-out.se>
2012-10-02 18:53:16 +0200

    Now all commands are executed by Tempo listening to editor commands, adding them to VersionModel and calling switchVersion

--- src/se/findout/tempo/client/VersionView.java
+++ src/se/findout/tempo/client/VersionView.java
@@ -93,6 +93,7 @@ public class VersionView extends FlowPanel implements VersionChangeListener {
 	}
 	
 	public void selectVersion(Version version) {
+		System.out.println("VersionView.selectVersion(" + version + ")");
 		if (version != selectedVersion) {
 			if (version == null) {
 				if (selectedVersionCircle != null) {
